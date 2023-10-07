78b4bb9d71b6aac0820145e3103f2b0f54c9c884
Anoosha <116190024+velagana@users.noreply.github.com>
2023-10-07 16:01:40 +0530

    Add files via upload

---
+++ monotonic.py
@@ -0,0 +1,9 @@
+arr = [9, 4, 5, 3, 7]
+
+# Sort the original list in ascending order using the sort() method
+arr.sort()
+
+# Reverse the sorted list in-place using the reverse() method
+arr.reverse()
+
+print(arr)
